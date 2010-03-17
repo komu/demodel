add9992d2b27dbf6dfac9ebaa2ccde48193f345c
Juha Komulainen <komu@iki.fi>
2010-03-18 00:15:49 +0200

    refactoring the matrix drawing code

---
+++ src/main/java/komu/demodel/ui/MatrixDrawer.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright (C) 2010 Juha Komulainen. All rights reserved.
+ */
+package komu.demodel.ui;
+
+import static java.awt.RenderingHints.KEY_TEXT_ANTIALIASING;
+import static java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+
+import komu.demodel.domain.Module;
+
+final class MatrixDrawer implements FontMetricsProvider {
+    
+    private final DependencyMatrixViewModel model;
+    private final Graphics2D g;
+    private final MatrixMetrics metrics;
+    private static final Color textColor = Color.BLACK;
+    private static final Color headerBackground = new Color(200, 200, 255);
+    private static final Color headerBackgroundSelected = new Color(200, 200, 255).brighter();
+    private static final Color gridColor = new Color(100, 100, 140);
+    private static final Color violationColor = Color.RED;
+    private static final Color backgroundColor = Color.WHITE;
+
+    MatrixDrawer(DependencyMatrixViewModel model, Graphics2D g) {
+        assert model != null;
+        assert g != null;
+        
+        this.model = model;
+        this.g = g;
+        this.metrics = new MatrixMetrics(model, this);
+    }
+    
+    @Override
+    public FontMetrics getFontMetrics(Font font) {
+        return g.getFontMetrics(font);
+    }
+   
+    void paintMatrix() {
+        g.setRenderingHint(KEY_TEXT_ANTIALIASING, VALUE_TEXT_ANTIALIAS_ON);
+        
+        drawBackground();
+        
+        drawLeftModuleList();
+        drawTopBar();
+        
+        drawGridLines();
+
+        drawDependencyStrengths();
+        drawViolations();
+        drawDiagonal();
+    }
+
+    private void drawBackground() {
+        g.setPaint(backgroundColor);
+        g.fill(g.getClipBounds());
+    }
+    
+    // Draw both the horizontal and vertical lines of the grid.
+    private void drawGridLines() {
+        int moduleCount = model.getVisibleModuleCount();
+        int leftWidth = metrics.getLeftHeaderWidth();
+        int cellHeight = metrics.getCellHeight();
+        int cellWidth = metrics.getCellWidth();
+        Dimension gridSize = metrics.getGridSize();
+        
+        g.setColor(gridColor);
+        g.drawLine(0, cellHeight, 0, gridSize.height);
+        g.drawLine(leftWidth, 0, gridSize.width, 0);
+
+        for (int mod = 0; mod <= moduleCount; mod++) {
+            int xx = leftWidth + (mod * cellWidth);
+            int yy = ((mod + 1) * cellHeight);
+            
+            g.drawLine(xx, 0, xx, gridSize.height);
+            g.drawLine(0, yy, gridSize.width, yy);
+        }
+    }
+
+    private void drawDependencyStrengths() {
+        int cellHeight = metrics.getCellHeight();
+        int cellWidth = metrics.getCellWidth();
+        int gridX = metrics.getLeftHeaderWidth();
+        int gridY = cellHeight;
+
+        int moduleCount = model.getVisibleModuleCount();
+        
+        FontMetrics gfm = metrics.getGridFontMetrics();
+        
+        // Draw the dependency strengths
+        g.setFont(metrics.getGridFont());
+        g.setColor(textColor);
+        int gridFontHeight = gfm.getHeight();
+        for (int row = 0; row < moduleCount; row++) {
+            for (int col = 0; col < moduleCount; col++) {
+                int strength = model.dependencyStrength(col, row);
+                if (row != col && strength > 0) {
+                    String str = String.valueOf(strength);
+                    int sw = gfm.stringWidth(str);
+                    
+                    int xx = gridX + (col * cellWidth) + ((cellWidth - sw) / 2);
+                    int yy = gridY + ((row + 1) * cellHeight) - ((cellHeight - gridFontHeight) / 2) - gfm.getDescent();
+                    g.drawString(str, xx + 1, yy);
+                }
+            }
+        }
+    }
+
+    private void drawDiagonal() {
+        int leftWidth = metrics.getLeftHeaderWidth();
+        int cellHeight = metrics.getCellHeight();
+        int cellWidth = metrics.getCellWidth();
+        int moduleCount = model.getVisibleModuleCount();
+
+        g.setColor(headerBackground);
+        for (int mod = 0; mod < moduleCount; mod++) {
+            int xx = leftWidth + (mod * cellWidth);
+            int yy = ((mod + 1) * cellHeight);
+            
+            g.fillRect(xx + 1, yy + 1, cellWidth - 1, cellHeight - 1);
+        }
+    }
+
+    private void drawViolations() {
+        int cellHeight = metrics.getCellHeight();
+        int cellWidth = metrics.getCellWidth();
+        int leftWidth = metrics.getLeftHeaderWidth();
+        int moduleCount = model.getVisibleModuleCount();
+        
+        int[] xs = new int[3];
+        int[] ys = new int[3];
+        
+        g.setColor(violationColor);
+        for (int row = 0; row < moduleCount; row++) {
+            for (int col = 0; col < moduleCount; col++) {
+                if (containsViolation(col, row)) {
+                    int xx = leftWidth + ((col + 1) * cellWidth);
+                    int yy = ((row + 1) * cellHeight) + 1;
+                    xs[0] = xx;
+                    xs[1] = xx - cellWidth/3;
+                    xs[2] = xx;
+                    ys[0] = yy;
+                    ys[1] = yy;
+                    ys[2] = yy + cellHeight/3;
+                    
+                    g.fillPolygon(xs, ys, 3);
+                }
+            }
+        }
+    }
+
+    private void drawLeftModuleList() {
+        int leftWidth= metrics.getLeftHeaderWidth();
+        int cellHeight = metrics.getCellHeight();
+        int moduleCount = model.getVisibleModuleCount();
+        int gridY = cellHeight;
+
+        // Paint background for module list
+        g.setColor(headerBackground);
+        g.fillRect(0, gridY, leftWidth, moduleCount * cellHeight);
+
+        int headerYOffset = metrics.getHeaderYOffset();
+
+        FontMetrics hfm = metrics.getHeaderFontMetrics();
+        int textdx = (metrics.getCellWidth() - hfm.charWidth('0')) / 2;
+        
+        g.setFont(metrics.getHeaderFont());
+        
+        int moduleIndex = 0;
+        for (Module module : model.getVisibleModules()) {
+            // draw background of selected module differently
+            if (module == model.getSelectedModule()) {
+                g.setColor(headerBackgroundSelected);
+                g.fillRect(0, gridY + moduleIndex * cellHeight, leftWidth, cellHeight);
+            }
+            
+            int moduleNumber = moduleIndex+1;
+            String num = String.valueOf(moduleNumber);
+            int numWidth = hfm.stringWidth(num);
+            int yy = headerYOffset + (moduleNumber * cellHeight);
+            
+            g.setColor(textColor);
+
+            int xx = metrics.getModuleDepthDx(module) + textdx;
+            if (model.isOpened(module))
+                g.drawString("-", xx, yy);
+            else if (!module.isLeaf())
+                g.drawString("+", xx, yy);
+
+            g.drawString(module.getLocalName(), MatrixMetrics.PLUS_WIDTH + xx, yy);
+            g.drawString(num, leftWidth - numWidth - textdx, yy);
+            
+            moduleIndex++;
+        }
+    }
+
+    private void drawTopBar() {
+        int cellHeight = metrics.getCellHeight();
+        int cellWidth = metrics.getCellWidth();
+        int leftHeader = metrics.getLeftHeaderWidth();
+        int moduleCount = model.getVisibleModuleCount();
+
+        // draw the background
+        g.setColor(headerBackground);
+        g.fillRect(leftHeader, 0, metrics.getGridSize().width - leftHeader, cellHeight);
+        
+        g.setFont(metrics.getHeaderFont());
+        g.setColor(textColor);
+
+        FontMetrics hfm = metrics.getHeaderFontMetrics();
+        int descent = hfm.getDescent();
+
+        for (int moduleIndex = 0; moduleIndex < moduleCount; moduleIndex++) {
+            int x = leftHeader + (moduleIndex * cellWidth);
+            
+            String str = String.valueOf(moduleIndex+1);
+            int strWidth = hfm.stringWidth(str);
+            if (strWidth < cellWidth) {
+                int textdx = (cellWidth-strWidth)/2;
+                g.drawString(str, x + textdx, cellHeight - descent);
+
+            } else {
+                // TODO: draw some marker
+                g.drawString(str, x, cellHeight - descent);
+            }
+        }
+    }
+    
+    private boolean containsViolation(int from, int to) {
+        return from > to && model.dependencyStrength(from, to) > 0;
+    }
+}
---
+++ src/main/java/komu/demodel/ui/MatrixMetrics.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2010 Juha Komulainen. All rights reserved.
+ */
+package komu.demodel.ui;
+
+import static java.lang.Math.max;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+
+import komu.demodel.domain.Module;
+
+final class MatrixMetrics {
+    private final DependencyMatrixViewModel model;
+    private final FontMetrics headerFontMetrics;
+    private final FontMetrics gridFontMetrics;
+    private int leftHeaderWidth = -1;
+    private Dimension gridSize;
+    private static final Font headerFont = new Font("dialog", Font.PLAIN, 12);
+    private static final Font gridFont = new Font("dialog", Font.PLAIN, 11);
+    private static final int DEPTH_DELTA = 10;
+    static final int PLUS_WIDTH = 20;
+
+    MatrixMetrics(DependencyMatrixViewModel model, FontMetricsProvider fontMetricsProvider) {
+        assert model != null;
+        assert fontMetricsProvider != null;
+        
+        this.model = model;
+        this.headerFontMetrics = fontMetricsProvider.getFontMetrics(headerFont);
+        this.gridFontMetrics = fontMetricsProvider.getFontMetrics(gridFont);
+    }
+    
+    int getLeftHeaderWidth() {
+        if (leftHeaderWidth == -1) {
+            int leftWidth = 20;
+            int extraWidth = 2 * headerFontMetrics.stringWidth(" 999");
+            
+            for (Module module : model.getVisibleModules())
+                leftWidth = max(leftWidth, headerFontMetrics.stringWidth(module.getLocalName()) + getModuleDepthDx(module));
+            leftHeaderWidth = PLUS_WIDTH + leftWidth + extraWidth;
+        }
+        return leftHeaderWidth;
+    }
+    
+    int getModuleDepthDx(Module module) {
+        return module.getDepth() * DEPTH_DELTA;
+    }
+    
+    Font getHeaderFont() {
+        return headerFont;
+    }
+    
+    FontMetrics getHeaderFontMetrics() {
+        return headerFontMetrics;
+    }
+
+    Font getGridFont() {
+        return gridFont;
+    }
+
+    FontMetrics getGridFontMetrics() {
+        return gridFontMetrics;
+    }
+    
+    Dimension getGridSize() {
+        if (gridSize == null) {
+            int moduleCount = model.getVisibleModuleCount();
+            
+            int gridWidth = getLeftHeaderWidth() + moduleCount * getCellWidth();
+            int gridHeight = (moduleCount + 1) * getCellHeight();
+            
+            gridSize = new Dimension(gridWidth, gridHeight);
+        }
+        return gridSize;
+    }
+    
+    int getHeaderYOffset() {
+        int cellHeight = getCellHeight();
+        
+        return cellHeight - ((cellHeight - headerFontMetrics.getHeight()) / 2) - headerFontMetrics.getDescent();
+    }
+
+    int getCellHeight() {
+        return (4 * headerFontMetrics.getHeight()) / 3;
+    }
+    
+    int getCellWidth() {
+        return getCellHeight();
+    }
+    
+    Module findModuleAt(int x, int y) {
+        int cellHeight = getCellHeight();
+        
+        int ypos = y - cellHeight;
+        if (x >= 0 && x <= getLeftHeaderWidth() && ypos >= 0) {
+            int moduleIndex = ypos / cellHeight;
+            if (moduleIndex < model.getVisibleModuleCount())
+                return model.getModuleAt(moduleIndex);
+        }
+        
+        return null;
+    }
+}
