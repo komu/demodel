d3ee8efe2fb10324c0a0de0971c64366748ec175
Juha Komulainen <komu@iki.fi>
2010-03-20 19:42:22 +0200

    added some sanity checks

--- src/main/java/komu/demodel/domain/Dependency.java
+++ src/main/java/komu/demodel/domain/Dependency.java
@@ -1,13 +1,15 @@
 package komu.demodel.domain;
 
-public class Dependency {
+public final class Dependency {
 
     //TODO: it would be nice to store more info
     private final Module to;
     private final DependencyType type;
     
     public Dependency(Module to, DependencyType type) {
-        super();
+        if (to == null) throw new NullPointerException("null to");
+        if (type == null) throw new NullPointerException("null type");
+
         this.to = to;
         this.type = type;
     }
