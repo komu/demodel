dc45a05e8bd0a5e3420980e05334a487d25d987a
Juha Komulainen <komu@iki.fi>
2010-03-23 17:51:32 +0200

    Show the module details in nicer layout instead of just dumping them to textarea.

--- src/main/java/komu/demodel/ui/ModuleDetailsView.java
+++ src/main/java/komu/demodel/ui/ModuleDetailsView.java
@@ -3,12 +3,10 @@
  */
 package komu.demodel.ui;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.text.NumberFormat;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -18,21 +16,39 @@ import komu.demodel.domain.PackageModule;
 import komu.demodel.ui.model.DependencyMatrixViewModel;
 import komu.demodel.utils.Check;
 
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+
 public final class ModuleDetailsView extends JPanel {
     
     private final DependencyMatrixViewModel model;
-    private final JTextArea textArea = new JTextArea();
+    private final JLabel numberOfTypes = new JLabel();
+    private final JLabel numberOfAbstractTypes = new JLabel();
+    private final JLabel afferentCouplings = new JLabel();
+    private final JLabel efferentCouplings = new JLabel();
+    private final JLabel abstractness = new JLabel();
+    private final JLabel instability = new JLabel();
+    private final JLabel distance = new JLabel();
     
     public ModuleDetailsView(DependencyMatrixViewModel model) {
         Check.notNull(model, "model");
         this.model = model;
         
-        setLayout(new BorderLayout());
-        add(new JScrollPane(textArea), BorderLayout.CENTER);
+        FormLayout layout = new FormLayout("max(40dlu;p), 8dlu, 20dlu", "");
+        DefaultFormBuilder builder = new DefaultFormBuilder(layout);
+        builder.setDefaultDialogBorder();
+        
+        builder.append("Number of Types:", numberOfTypes);
+        builder.append("Number of Abstract Types:", numberOfAbstractTypes);
+        builder.append("Afferent Couplings:", afferentCouplings);
+        builder.append("Efferent Couplings:", efferentCouplings);
+        builder.append("Abstractness:", abstractness);
+        builder.append("Instability:", instability);
+        builder.append("Distance: ", distance);
+        
+        add(builder.getPanel());
         
         model.addListener(new MyModelListener());
-        setPreferredSize(new Dimension(300, 300));
-        
         updateViewFromSelectedModule();
     }
     
@@ -41,18 +57,23 @@ public final class ModuleDetailsView extends JPanel {
         if (module instanceof PackageModule) {
             PackageMetrics metrics = ((PackageModule) module).getMetrics();
             
-            StringBuilder text = new StringBuilder();
-            text.append("Number of Types: ").append(metrics.getNumberOfTypes()).append("\n");
-            text.append("Number of Abstract Types: ").append(metrics.getNumberOfAbstractTypes()).append("\n");
-            text.append("Afferent Couplings: ").append(metrics.getAfferentCouplings()).append("\n");
-            text.append("Efferent Couplings: ").append(metrics.getEfferentCouplings()).append("\n");
-            text.append("Abstractness: ").append(metrics.getAbstractness()).append("\n");
-            text.append("Instability: ").append(metrics.getInstability()).append("\n");
-            text.append("Distance from the Main Sequence: ").append(metrics.getDistanceFromMainSequence()).append("\n");
-
-            textArea.setText(text.toString());
+            NumberFormat numberFormat = NumberFormat.getInstance();
+            
+            numberOfTypes.setText(numberFormat.format(metrics.getNumberOfTypes()));
+            numberOfAbstractTypes.setText(numberFormat.format(metrics.getNumberOfAbstractTypes()));
+            afferentCouplings.setText(numberFormat.format(metrics.getAfferentCouplings()));
+            efferentCouplings.setText(numberFormat.format(metrics.getEfferentCouplings()));
+            abstractness.setText(numberFormat.format(metrics.getAbstractness()));
+            instability.setText(numberFormat.format(metrics.getInstability()));
+            distance.setText(numberFormat.format(metrics.getDistanceFromMainSequence()));
         } else {
-            textArea.setText("");
+            numberOfTypes.setText("-");
+            numberOfAbstractTypes.setText("-");
+            afferentCouplings.setText("-");
+            efferentCouplings.setText("-");
+            abstractness.setText("-");
+            instability.setText("-");
+            distance.setText("-");
         }
     }
 
