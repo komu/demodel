a9246e11e53ee27ee97325c8a5c570278e0ed99f
Juha Komulainen <komu@iki.fi>
2010-10-01 12:16:41 +0300

    fixed calculation of cyclomatic complexity

--- src/main/scala/komu/demodel/parser/java/CyclomaticComplexity.scala
+++ src/main/scala/komu/demodel/parser/java/CyclomaticComplexity.scala
@@ -8,41 +8,35 @@ object CyclomaticComplexity {
   /**
    * Calculates the cyclomatic complexity of given method.
    */
-  def cyclomaticComplexity(owner: String, mn: MethodNode) {
+  def cyclomaticComplexity(owner: String, mn: MethodNode): Int = {
     val a = new Analyzer(new BasicInterpreter) {
       override def newFrame(nLocals: Int, nStack: Int) = 
-        new Node(nLocals, nStack)
+        new Frame(nLocals, nStack) with Successors
       
       override def newFrame(src: Frame) = 
-        new Node(src)
+        new Frame(src) with Successors
       
       override def newControlFlowEdge(src: Int, dst: Int) =
         nodeAt(src).addSuccessor(nodeAt(dst))
       
       private def nodeAt(index: Int) = 
-        getFrames()(index).asInstanceOf[Node]
+        getFrames()(index).asInstanceOf[Successors]
     }
     
-    a.analyze(owner, mn);
+    a.analyze(owner, mn)
     
     val frames = a.getFrames.filter(f => f != null)
     var nodes = frames.length
-    val edges = frames.map(f => f.asInstanceOf[Node].successorCount).sum
+    val edges = frames.map(f => f.asInstanceOf[Successors].successorCount).sum
     
-    return edges - nodes + 2;
+    edges - nodes + 2;
   }
 }
 
-class Node(nLocals: Int, nStack: Int) extends Frame(nLocals, nStack) {
+trait Successors {
+  private var successors = Set[Object]()
   
-  private var successors = Set[Node]()
-  
-  def this(src: Frame) {
-    this(0, 0);
-    init(src)
-  }
-  
-  def addSuccessor(node: Node) {
+  def addSuccessor(node: Object) {
     successors = successors + node
   }
   
